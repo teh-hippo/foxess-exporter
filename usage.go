6ae22af51252bd62f6f798d7b478cfe4ab0be57c
teh-hippo <490309+teh-hippo@users.noreply.github.com>
2024-12-09 11:41:42 +1100

    Allow api usage to be retrieved from other commandts

--- usage.go
+++ usage.go
@@ -12,11 +12,13 @@ type ApiUsageCommand struct {
 }
 
 type AccessCountResponse struct {
-	ErrorNumber int `json:"errno"`
-	Result      struct {
-		Total     json.Number `json:"total"`
-		Remaining json.Number `json:"remaining"`
-	} `json:"result"`
+	ErrorNumber int      `json:"errno"`
+	Result      ApiUsage `json:"result"`
+}
+
+type ApiUsage struct {
+	Total     json.Number `json:"total"`
+	Remaining json.Number `json:"remaining"`
 }
 
 var apiUsageCommand ApiUsageCommand
@@ -26,27 +28,36 @@ func init() {
 }
 
 func (x *ApiUsageCommand) Execute(args []string) error {
-	response := &AccessCountResponse{}
-	err := foxess.NewRequest(options.ApiKey, "GET", "/op/v0/user/getAccessCount", nil, response, options.Debug)
+	apiUsage, err := GetApiUsage()
 	if err != nil {
-		return err
-	}
-
-	if err = foxess.IsError(response.ErrorNumber, ""); err != nil {
 		return err
 	}
 
 	tbl := table.New("Total", "Remaining", "Used")
 	var total, remaining float64
-	if total, err = response.Result.Total.Float64(); err != nil {
+	if total, err = apiUsage.Total.Float64(); err != nil {
 		return err
 	}
-	if remaining, err = response.Result.Remaining.Float64(); err != nil {
+	if remaining, err = apiUsage.Remaining.Float64(); err != nil {
 		return err
 	}
 	percentage := (total - remaining) / total * 100
-	tbl.AddRow(response.Result.Total, response.Result.Remaining, fmt.Sprintf("%.2f%%", percentage))
+	tbl.AddRow(apiUsage.Total, apiUsage.Remaining, fmt.Sprintf("%.2f%%", percentage))
 	tbl.Print()
 
 	return nil
+}
+
+func GetApiUsage() (*ApiUsage, error) {
+	response := &AccessCountResponse{}
+	err := foxess.NewRequest(options.ApiKey, "GET", "/op/v0/user/getAccessCount", nil, response, options.Debug)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = foxess.IsError(response.ErrorNumber, ""); err != nil {
+		return nil, err
+	}
+
+	return &response.Result, nil
 }
